ff527b2289eadda7d157b84f5583d61f43bda97a
Alina Pak <47726938+avpa11@users.noreply.github.com>
2023-07-20 15:25:01 -0700

    Update project.asm

--- project.asm
+++ project.asm
@@ -1,13 +1,16 @@
 ; COMP 2825 Group Project
 ; Summer 2023 term
 
+; system call numbers
 SYS_EXIT  equ 1
 SYS_READ  equ 3
 SYS_WRITE equ 4
+; file descriptors
 STDIN     equ 0
 STDOUT    equ 1
 
 
+; declaring and initializes messages displayed to a user
 section .data
    msg db 'A sum of any two single digit integers', 0xa, 0xD
    len equ $ - msg
@@ -24,7 +27,7 @@ section .data
    msg_newline db 0xa
    len_newline equ $ - msg_newline
 
-
+; declaring variables for 2 user inputs and 1 result
 section .bss
    num1 resb 2 
    num2 resb 2 
@@ -35,37 +38,43 @@ section .text
    global _start    
 
 	
-_start:             
+_start:
+   ; display the first message "A sum of any two single digit integers"
    mov	edx,len     
    mov	ecx,msg     
    mov	ebx,1       
    mov	eax,4       
    int	0x80        
 
+   ; display the message asking for the first user input
    mov eax, SYS_WRITE         
    mov ebx, STDOUT         
    mov ecx, msg1         
    mov edx, len1 
    int 0x80                
 
+   ; read the first user input
    mov eax, SYS_READ 
    mov ebx, STDIN  
    mov ecx, num1 
    mov edx, 2
    int 0x80            
 
+   ; display the message asking for the second user input
    mov eax, SYS_WRITE        
    mov ebx, STDOUT         
    mov ecx, msg2          
    mov edx, len2         
    int 0x80
 
+   ; read the second user input
    mov eax, SYS_READ  
    mov ebx, STDIN  
    mov ecx, num2 
    mov edx, 2
    int 0x80        
 
+   ; the inputs are retrieved, converted from ASCII to integer form, and added together
    mov al, byte [num1]
    sub al, '0'
 
@@ -74,13 +83,16 @@ _start:
 
    add al, bl
 
+   ; compare the sum in al to 9
    cmp al, 9
    jbe single_digit
 
+   ; for double digit sum
    add al, '0' - 10
    mov byte [res], '1' 
    mov byte [res+1], al 
 
+   ; display the message for the sum
    mov eax, SYS_WRITE        
    mov ebx, STDOUT
    mov ecx, msg3         
@@ -93,12 +105,14 @@ _start:
    mov edx, 2        
    int 0x80
 
+   ; print the new line message
    mov eax, SYS_WRITE        
    mov ebx, STDOUT
    mov ecx, msg_newline        
    mov edx, len_newline      
    int 0x80
 
+   ; exit
    mov eax, SYS_EXIT   
    xor ebx, ebx 
    int 0x80
@@ -107,6 +121,7 @@ single_digit:
    add al, '0'
    mov byte [res], al
 
+   ; display the message for the sum
    mov eax, SYS_WRITE        
    mov ebx, STDOUT
    mov ecx, msg3         
@@ -119,12 +134,14 @@ single_digit:
    mov edx, 1        
    int 0x80
 
+   ; print the new line message
    mov eax, SYS_WRITE        
    mov ebx, STDOUT
    mov ecx, msg_newline        
    mov edx, len_newline      
    int 0x80
 
+   ; exit
    mov eax, SYS_EXIT   
    xor ebx, ebx 
    int 0x80
